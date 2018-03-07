96a697c33c7a95e8f50752b348813bf6aa449868
Michal84B <banbura.michal@gmail.com>
2018-03-07 18:47:06 +0100

    Second Commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -7,4 +7,12 @@ public class Calculator {
     public int substract(int a, int b) {
         return a - b;
     }
+    public static void main(String args[]) {
+        Calculator calculator = new Calculator();
+        int result = calculator.sum(5, 7);
+        System.out.println("Wynik dodawania " + result);
+
+        int result2 = calculator.substract(result, 3);
+        System.out.println("Wynik odejmowania " + result2);
+    }
 }
